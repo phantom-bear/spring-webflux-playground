3e20ba82949c648a2702982522c1f7c7715224d8
phantom-bear <phantombear6650@gamil.com>
2024-09-15 12:04:02 +0800

    add init git files

---
+++ src/test/java/com/vinsguru/webflux_playground/WebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinsguru.webflux_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
